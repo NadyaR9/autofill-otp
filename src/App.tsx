79b7fffafa0d03053146c080f53cc82e0d8652a8
Nadezhda Razinkova <nrazinkova@roox.ru>
2025-06-01 13:01:32 +0300

    update opt

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { OTPInput } from './Otp'
 
 function App() {
 
-  return <></>
+  return (
+    <div style="border:1px solid; padding: 5px; 10px; margin: 10px 0;">
+      <form action="/post" method="post">
+        Enter OTP here:
+        <input type="text" autocomplete="one-time-code" inputmode="numeric" name="one-time-code" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
 }
 
 export default App
